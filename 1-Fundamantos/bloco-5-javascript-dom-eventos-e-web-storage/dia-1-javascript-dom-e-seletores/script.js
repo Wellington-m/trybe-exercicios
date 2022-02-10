79fce390e03e9920e275de025c08131504f8cc38
Wellington <wellingtonmachioni@gmail.com>
2022-02-10 15:16:18 -0300

    Exercicios 1 ao 4

---
+++ 1-Fundamantos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -0,0 +1,7 @@
+document.getElementsByTagName("p")[0].innerText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit asperiores nisi id fugit doloribus, fuga nihil delectus dolor ratione aspernatur dolores maiores corporis recusandae dolorem laboriosam iusto soluta impedit mollitia?"
+
+document.getElementsByClassName("main-content")[0].style.backgroundColor = "rgb(76,164,109)";
+
+document.getElementsByClassName("center-content")[0].style.backgroundColor = "white";
+
+document.getElementsByTagName("h1")[0].innerText = "Exercício 5.1 - JavaScript";
