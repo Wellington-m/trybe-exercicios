0778e8317e03eef17d7dc4cbf6ba16b44bab5099
Wellington <wellingtonmachioni@gmail.com>
2022-02-10 20:58:57 -0300

    ADicionado tag main

--- 1-Fundamantos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ 1-Fundamantos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -1,7 +1,9 @@
 let body = document.body;
 
 let h1 = document.createElement('h1');
-
 h1.innerText = "Exercício 5.2 - JavaScript DOM";
-
 body.appendChild(h1);
+
+let main = document.createElement('main');
+main.className = 'main-content';
+body.appendChild(main);
