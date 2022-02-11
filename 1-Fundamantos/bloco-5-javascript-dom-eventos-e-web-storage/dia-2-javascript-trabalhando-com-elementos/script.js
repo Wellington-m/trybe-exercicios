ed5eea37e376ca528d547f8373e08ea4ebfc7467
Wellington <wellingtonmachioni@gmail.com>
2022-02-10 21:10:30 -0300

    Adicionado tag p com um texto

--- 1-Fundamantos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ 1-Fundamantos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -1,13 +1,26 @@
+function createElementByClass(elementName, className)
+{
+    let element = document.createElement(elementName);
+    element.className = className;
+    return element;
+}
+function createElementByText(elementName, text)
+{
+    let element = document.createElement(elementName);
+    element.innerText = text;
+    return element;
+}
+
 let body = document.body;
 
-let h1 = document.createElement('h1');
-h1.innerText = "Exercício 5.2 - JavaScript DOM";
+let h1 = createElementByText('h1', 'Exercício 5.2 - JavaScript DOM');
 body.appendChild(h1);
 
-let main = document.createElement('main');
-main.className = 'main-content';
+let main = createElementByClass('main', 'main-content');
 body.appendChild(main);
 
-let section = document.createElement('section');
-section.className = 'center-content';
+let section = createElementByClass('section', 'center-content');
 main.appendChild(section);
+
+let p = createElementByText('p', "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.");
+section.appendChild(p);
