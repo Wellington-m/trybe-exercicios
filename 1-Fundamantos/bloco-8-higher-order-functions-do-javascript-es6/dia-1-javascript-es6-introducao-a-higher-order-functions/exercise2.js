a7b82ba97c2291e0a74a1e4b0af45c565bf143eb
Wellington <wellingtonmachioni@gmail.com>
2022-03-09 19:19:08 -0300

    Implementado exercicio 2

--- 1-Fundamantos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercise2.js
+++ 1-Fundamantos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercise2.js
@@ -2,3 +2,23 @@
 
 //1- Desenvolver uma HOF
 //2- Ela precisa retornar o resultado de um sorteio
+//3- HOF irá gerar um número aleatório entre 1 e 5
+//4- HOF vai receber como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado.
+//5 - A HOF deve retornar (Ex: "Tente novamente" ou "Parabéns você ganhou").
+
+const getRandomIntInclusive = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+const checkNumber = (drawnNumber, chosenNumber) => (drawnNumber === chosenNumber) ? true : false;
+
+const draw = (number, callback) => {
+    let numberDrawn = getRandomIntInclusive(1,5);
+    console.log(numberDrawn);
+
+    return (callback(numberDrawn, number) === true) ? 'Parabéns você ganhou' : 'Tente novamente';
+}
+
+console.log(draw(5, checkNumber));
