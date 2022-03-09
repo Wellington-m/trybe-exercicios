696dfd894c757339ec86530971f83afcfeefcf01
Wellington <wellingtonmachioni@gmail.com>
2022-03-09 18:46:27 -0300

    Implementado exercicio 1

--- 1-Fundamantos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercise1.js
+++ 1-Fundamantos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercise1.js
@@ -1,16 +1,24 @@
 // 1 - Crie uma função que retorne um objeto no formato { nomeCompleto, email } representando uma nova pessoa contratada. Passe sua função como parâmetro da HOF newEmployees para criar cada pessoa contratada em seu respectivo id . A sua função deve receber como parâmetro o nome completo da pessoa funcionária e a partir dele gerar automaticamente um email no formato nome_da_pessoa@trybe.com .
 
-const newEmployees = () => {
+const newEmployees = (callback) => {
     const employees = {
-      id1: '', // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-      id2: '', // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-      id3: '', // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+        id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+        id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+        id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
     }
     return employees;
-  };
+};
 
-  //1- Criar uma função
-  //2- A função deve retornar um objeto no formato { nomeCompleto, email }
-  //3- Passar a função como parâmetro da HOF newEmployees
-  //4- A função que eu criar deve receber um nome completo como parâmetro
-  //5- A função deve criar um e-mail no formato nome_da_pessoa@trybe.com
+//1- Criar uma função
+//2- A função deve retornar um objeto no formato { nomeCompleto, email }
+//3- Passar a função como parâmetro da HOF newEmployees
+//4- A função que eu criar deve receber um nome completo como parâmetro
+//5- A função deve criar um e-mail no formato nome_da_pessoa@trybe.com
+
+function createEmail(fullName) {
+    const fullNameLower = fullName.toLowerCase().replace(' ', '_');
+    const email = `${fullNameLower}@trybe.com`;
+    return { fullName, email };
+}
+
+console.log(newEmployees(createEmail)); 
