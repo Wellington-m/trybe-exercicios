49f43d4887677609c0a180e8b4180276ed28d72e
Wellington <wellingtonmachioni@gmail.com>
2022-03-21 20:30:01 -0300

    Implementado exercicio 7 e teste dele

--- 1-Fundamantos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercise7.js
+++ 1-Fundamantos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercise7.js
@@ -2,6 +2,8 @@
 
 const uppercase = (str, callback) => {
     setTimeout(() => {
-      callback(str.toUpperCase());
+        callback(str.toUpperCase());
     }, 500);
-  };
+};
+
+module.exports = uppercase;
